116949cfcb4f6a630fe28565a82ac74d380cc8c9
czx.me <heycmm.cn@gmail.com>
2022-02-12 11:21:27 +0800

    :hammer: 摄像头获取

--- camera/camera.py
+++ camera/camera.py
@@ -1,17 +1,27 @@
+import platform
+
 import cv2 as cv
 import numpy as np
 import base64
 from PIL import Image
+
 """
 摄像头和转码base64，相当于动画片
 """
+
+
 class Camera:
     def __init__(self):
+
         self.__cap = cv.VideoCapture(0)
         self.__JPEG_HEADER = "data:image/jpeg;base64,"
 
-
     def read(self):
+        if platform.system() == 'Linux':
+            self.__cap = cv.VideoCapture(10)
+        else:
+            self.__cap = cv.VideoCapture(0)
+
         ret, frame = self.__cap.read()
         r, buf = cv.imencode(".jpeg", frame)
         dat = Image.fromarray(np.uint8(buf)).tobytes()
